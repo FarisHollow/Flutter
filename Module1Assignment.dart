b0ed8a9f40719c1b7239705937af3cef04aefee3
FarisHollow <mirfaris79@gmail.com>
2023-04-15 15:55:05 +0600

    Assignment 1 Module 1 Dart

---
+++ Module1Assignment.dart
@@ -0,0 +1,14 @@
+///Take a list of phone numbers.
+///write program that prints out the full number like ‘+8801768171985’ You have to print out all numbers given in the list except +88.
+
+void main(){
+
+  List<String> phoneNumber = [ "+88", "01768131685", "01768171985", "01768111286", "01768131685"];
+
+  print("+88"+phoneNumber[1]);
+  print("+88"+phoneNumber[2]);
+  print("+88"+phoneNumber[3]);
+  print("+88"+phoneNumber[4]);
+
+
+}
