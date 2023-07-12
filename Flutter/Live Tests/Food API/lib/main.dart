0d476ef4c5439c9917526773e9ec34d7f6e963a7
FarisHollow <mirfaris79@gmail.com>
2023-07-12 20:57:53 +0600

    Module 9 live test

---
+++ Flutter/Live Tests/Food API/lib/main.dart
@@ -0,0 +1,129 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(RecipeApp());
+}
+
+class RecipeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: RecipeScreen(),
+    );
+  }
+}
+
+class RecipeScreen extends StatefulWidget {
+  @override
+  _RecipeScreenState createState() => _RecipeScreenState();
+}
+
+class _RecipeScreenState extends State<RecipeScreen> {
+  List<Recipe> recipes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadRecipes();
+  }
+
+  void loadRecipes() {
+    // Parse the JSON string into a list of recipes
+    String jsonString = '''
+    {
+      "recipes": [
+        {
+          "title": "Pasta Carbonara",
+          "description": "Creamy pasta dish with bacon and cheese.",
+          "ingredients": ["spaghetti", "bacon", "egg", "cheese"]
+        },
+        {
+          "title": "Caprese Salad",
+          "description": "Simple and refreshing salad with tomatoes, mozzarella, and basil.",
+          "ingredients": ["tomatoes", "mozzarella", "basil"]
+        },
+        {
+          "title": "Banana Smoothie",
+          "description": "Healthy and creamy smoothie with bananas and milk.",
+          "ingredients": ["bananas", "milk"]
+        },
+        {
+          "title": "Chicken Stir-Fry",
+          "description": "Quick and flavorful stir-fried chicken with vegetables.",
+          "ingredients": ["chicken breast", "broccoli", "carrot", "soy sauce"]
+        },
+        {
+          "title": "Grilled Salmon",
+          "description": "Delicious grilled salmon with lemon and herbs.",
+          "ingredients": ["salmon fillet", "lemon", "olive oil", "dill"]
+        },
+        {
+          "title": "Vegetable Curry",
+          "description": "Spicy and aromatic vegetable curry.",
+          "ingredients": ["mixed vegetables", "coconut milk", "curry powder"]
+        },
+        {
+          "title": "Berry Parfait",
+          "description": "Layered dessert with fresh berries and yogurt.",
+          "ingredients": ["berries", "yogurt", "granola"]
+        }
+      ]
+    }
+    ''';
+
+    List<dynamic> jsonRecipes = jsonDecode(jsonString)['recipes'];
+
+    // Convert the JSON recipes into Recipe objects
+    List<Recipe> loadedRecipes = jsonRecipes
+        .map((jsonRecipe) => Recipe.fromJson(jsonRecipe))
+        .toList();
+
+    setState(() {
+      recipes = loadedRecipes;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recipe App'),
+      ),
+      body: ListView.builder(
+        itemCount: recipes.length,
+        itemBuilder: (context, index) {
+          Recipe recipe = recipes[index];
+          return ListTile(
+            title: Text(recipe.title),
+            subtitle: Text(recipe.description),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class Recipe {
+  final String title;
+  final String description;
+  final List<String> ingredients;
+
+  Recipe({
+    required this.title,
+    required this.description,
+    required this.ingredients,
+  });
+
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    return Recipe(
+      title: json['title'],
+      description: json['description'],
+      ingredients: List.from(json['ingredients']),
+    );
+  }
+}
